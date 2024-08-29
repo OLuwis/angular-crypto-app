4758679352df4a66df08969107f1fdbb6aedc275
Luis Miguel <oluismrs@gmail.com>
2024-08-29 20:40:43 -0300

    add header interceptor to http client

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,8 +4,15 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { HeaderInterceptor } from './interceptors/header.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), provideHttpClient(withFetch())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideAnimationsAsync(),
+    provideHttpClient(withFetch(), withInterceptors([HeaderInterceptor])),
+  ]
 };
