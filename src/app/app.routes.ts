ce38b7ac0f30a3271ebd25d353d23ccfb1008f2c
Luis Miguel <oluismrs@gmail.com>
2024-09-02 17:39:19 -0300

    add metrics route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { MetricsComponent } from './pages/metrics/metrics.component';
 
-export const routes: Routes = [{ path: "", component: HomeComponent }];
+export const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "/metrics/*", component: MetricsComponent }
+];
