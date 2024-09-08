c2d5af38361ff5399069c47e6896e457279a9735
Luis Miguel <oluismrs@gmail.com>
2024-09-07 23:30:12 -0300

    add env variable to header interceptor

--- src/app/interceptors/header.interceptor.ts
+++ src/app/interceptors/header.interceptor.ts
@@ -1,7 +1,8 @@
 import { HttpEvent, HttpHandlerFn, HttpRequest } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { environment } from "../../environments/environment";
 
 export function HeaderInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
-  const newReq = req.clone({ headers: req.headers.append("Authorization", "") });
+  const newReq = req.clone({ headers: req.headers.append("Authorization", environment.COINAPI_KEY) });
   return next(newReq)
 }
