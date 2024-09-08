e964a07bbacd80777b84ff9e168c1c6801503250
Luis Miguel <oluismrs@gmail.com>
2024-09-07 21:55:22 -0300

    create metrics page for assets

--- src/app/pages/metrics/metrics.component.ts
+++ src/app/pages/metrics/metrics.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { ChartComponent } from '../../components/chart/chart.component';
+import { CommonService } from '../../services/common.service';
 
 @Component({
   selector: 'app-metrics',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatGridListModule, ChartComponent],
   templateUrl: './metrics.component.html',
   styleUrl: './metrics.component.scss'
 })
 export class MetricsComponent {
+  @Input()
+  asset!: string
 
+  constructor(public commonService: CommonService) { }
 }
