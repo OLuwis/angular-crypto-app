b2b719150d71f72584a71d58b14bd77b991595a9
Luis Miguel <oluismrs@gmail.com>
2024-09-07 21:53:51 -0300

    add new routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { HomeComponent } from './pages/home/home.component';
 import { MetricsComponent } from './pages/metrics/metrics.component';
 
 export const routes: Routes = [
-  { path: "", component: HomeComponent },
-  { path: "/metrics/:asset", component: MetricsComponent }
+  { path: ":asset", component: MetricsComponent },
+  { path: "", component: HomeComponent }
 ];
