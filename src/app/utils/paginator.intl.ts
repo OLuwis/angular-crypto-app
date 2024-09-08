4b3800b8d747c353b611cd530bdd2a2f0f53c289
Luis Miguel <oluismrs@gmail.com>
2024-09-07 21:54:05 -0300

    update paginator labels

--- src/app/utils/paginator.intl.ts
+++ src/app/utils/paginator.intl.ts
@@ -5,16 +5,16 @@ import { Subject } from "rxjs";
 @Injectable({ providedIn: "root" })
 export class PaginatorIntl implements MatPaginatorIntl {
   changes = new Subject<void>();
-  itemsPerPageLabel: string = "Items por página";
-  nextPageLabel: string = "Próxima página";
-  previousPageLabel: string = "Página Anterior";
-  firstPageLabel: string = "Primeira Página";
-  lastPageLabel: string = "Ultimá Página";
+  itemsPerPageLabel: string = "Items Per Page";
+  nextPageLabel: string = "Next Page";
+  previousPageLabel: string = "Previous Page";
+  firstPageLabel: string = "";
+  lastPageLabel: string = "";
   getRangeLabel(page: number, pageSize: number, length: number): string {
     if (length === 0) {
-      return "Página 1 de 1"
+      return "Page 1 of 1"
     }
     const amountPages = Math.ceil(length / pageSize)
-    return `Página ${page + 1} of ${amountPages}`
+    return `Page ${page + 1} of ${amountPages}`
   };
 }
