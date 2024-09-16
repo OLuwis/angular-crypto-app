af712cc5f9427bcfd59fe6e9ee88adb6d0acb52b
Luis Miguel <oluismrs@gmail.com>
2024-09-16 16:01:13 -0300

    change api calls for client side

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, afterRender } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { MatPaginatorIntl, MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 import { MatGridListModule } from '@angular/material/grid-list';
-import { CardComponent } from '../../components/card/card.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatPaginatorIntl, MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { CardComponent } from '../../components/card/card.component';
 import { CommonService } from '../../services/common.service';
 import { CryptoService } from '../../services/crypto.service';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { FilterService } from '../../services/filter.service';
-import { AsyncPipe } from '@angular/common';
 import { PaginatorIntl } from '../../utils/paginator.intl';
 import { Icon } from '../../types/icon';
 
@@ -26,18 +26,22 @@ export class HomeComponent {
   filter = new FormControl("")
 
   constructor(public commonService: CommonService, public cryptoService: CryptoService, public filterService: FilterService) {
-    cryptoService.setIcons(64)
     cryptoService.setPaginatedIcons(commonService.getPageSizeValue())
     filterService.setPaginatedFilteredIcons(commonService.getPageSizeValue())
 
     this.filter.valueChanges.subscribe(value => {
       if (value) {
-        this.filterService.setFilteredIcons(cryptoService.getIconsValue(), value.toUpperCase())
+        this.filterService.setFilteredIcons(cryptoService.getIcons(), value.toUpperCase())
       }
     })
 
     this.commonService.getSaved().subscribe(saved => {
-      this.saved_icons = this.cryptoService.getIconsValue().filter(icon => saved.includes(icon.asset_id))
+      this.saved_icons = this.cryptoService.getIcons().filter(icon => saved.includes(icon.asset_id))
+    })
+
+    afterRender(() => {
+      cryptoService.setIcons(64)
+      cryptoService.setAssets()
     })
   }
 
