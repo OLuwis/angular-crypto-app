d8b7dec05dd7136cfe7bac5a8e52815693450e10
Luis Miguel <oluismrs@gmail.com>
2024-09-16 16:06:05 -0300

    trim asset type object data

--- src/app/types/asset.d.ts
+++ src/app/types/asset.d.ts
@@ -1,31 +1,4 @@
-import { UUID } from "crypto"
-
 export interface Asset {
   asset_id: string,
-  name: string,
-  type_is_crypto: number,
-  data_quote_start: Date,
-  data_quote_end: Date,
-  data_orderbook_start: Date,
-  data_orderbook_end: Date,
-  data_trade_start: Date,
-  data_trade_end: Date,
-  data_symbols_count: number,
-  volume_1hrs_usd: number,
-  volume_1day_usd: number,
-  volume_1mth_usd: number,
-  price_usd: number,
-  id_icon: UUID,
-  supply_current: number,
-  supply_total: number,
-  supply_max: number,
-  chain_addresses: [
-    {
-      chain_id: string,
-      network_id: string,
-      address: string
-    }
-  ],
-  data_start: string,
-  data_end: string
+  name: string
 }
